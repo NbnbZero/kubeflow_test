71a3e5bc1ebbd543496805e2d85a2181be4041c4
Chen Ling <chenling9727@gmail.com>
2021-05-28 11:44:52 +0800

    commit test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print('hello world')  # Press ⌘F8 to toggle the breakpoint.
+    print('?')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
