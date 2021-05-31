aabecdbe636de1bd38891b6b1df3d30064d0b266
Chen Ling <chenling9727@gmail.com>
2021-05-31 11:50:57 +0800

    commit test

--- main.py
+++ main.py
@@ -3,16 +3,32 @@ import sys
 # locally
 def hello_world_local():
     print("hello world local")
+    print('complete task')
 
 # with k8s
 def hello_world_k8s():
-    # set up k8s 
-    print("hello world k8s")
+    # set up k8s
+    import kfp
+    from kfp import components, dsl
+
+    @components.create_component_from_func
+    def get_op1():
+        print('hello world k8s')
+
+    @components.create_component_from_func
+    def get_op2():
+        print("complete task")
+
+    @dsl.pipeline(name='pipeline_hello_world')
+    def my_pipeline():
+        task1 = get_op1()
+        task2 = get_op2()
+        task2.after(task1)
+
+    kfp.compiler.Compiler().compile(my_pipeline, __file__ + '.yaml')
 
 if __name__ == '__main__':
-    if len(sys.argv)<2:
-        hello_world_local()
-    elif sys.argv[1] == "local":
+    if sys.argv[1] == "local":
         hello_world_local()
     elif sys.argv[1] == "k8s":
         hello_world_k8s()
