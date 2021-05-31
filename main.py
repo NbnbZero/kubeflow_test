87cc0152040d90249858a2bd8e881a8d7ced915b
Chen Ling <chenling9727@gmail.com>
2021-05-31 17:44:50 +0800

    Some revises

--- main.py
+++ main.py
@@ -1,50 +1,48 @@
 import sys
 
-# @components.create_component_from_func
-def op1():
+
+# define 1st op
+def func1():
     print("hello world")
 
-# @components.create_component_from_func
-def op2(device):
+
+# define 2nd op
+def func2(device):
     print("complete " + device + " task") 
 
 # locally
 def hello_world_local():
-    op1()
-    op2("local")
+    func1()
+    func2("local")
 
 
 # with k8s
 def hello_world_k8s():
-    # @dsl.pipeline(name='pipeline_hello_world')
+
     import kfp
     from kfp import components, dsl
+
+    # get containerOps from functions
     @components.create_component_from_func
     def get_op1():
-        op1()
+        func1()
 
     @components.create_component_from_func
     def get_op2():
-        op2("k8s")
+        func2("k8s")
 
     @dsl.pipeline(name='pipeline_hello_world')
     def my_pipeline():
-        # get_op1 = components.create_component_from_func(op1)
-        # get_op2 = components.create_component_from_func(op2)
         task1 = get_op1()
         task2 = get_op2()
-        # get_op2.after(get_op1)
         task2.after(task1)
 
     kfp.compiler.Compiler().compile(my_pipeline, __file__ + '.yaml')
 
 
 if __name__ == '__main__':
-    if len(sys.argv)<2:
+    device = sys.argv[1]
+    if device == "k8s":
+        hello_world_k8s()
+    elif device == "local":
         hello_world_local()
-    else:
-        device = sys.argv[1]
-        if device == "k8s":
-            hello_world_k8s()
-        elif device == "local":
-            hello_world_local()
